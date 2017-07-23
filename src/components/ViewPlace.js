4fbec2b930f789cfa979b6199acec14879964194
Andrew Zahler <andrew.zahler@gmail.com>
2017-07-23 12:14:41 -0400

    adding my branch

--- src/components/ViewPlace.js
+++ src/components/ViewPlace.js
@@ -6,22 +6,32 @@ import Modal from 'react-awesome-modal';
 export default class ViewPlace extends Component {
   constructor(props) {
       super(props);
+      this.queryPlaceDetails = this.queryPlaceDetails.bind(this);
       this.state = {
         visible : false,
-        detailedPlace: {}
+        beforeQuery: true,
+        detailedPlace: {},
+        photoUrls: []
       }
   }
 
   componentDidUpdate() {
-    if (this.state.detailedPlace = {}) {
-      this.props.queryPlaceDetails(this.props.placeId);
+    if (this.state.beforeQuery === true) {
+      this.queryPlaceDetails(this.props.placeId);
+      this.setState({
+        detailedPlace: this.state.detailedPlace,
+        beforeQuery: false
+      })
+    }
+    if (this.state.detailedPlace !== {} && this.state.beforeQuery === false) {
+      console.log(this.state.detailedPlace.photos[0]);
+      /*TODO: grab and store data from arrays in the detailedPlace object for rendering */ 
     }
   }
 
   openModal() {
       this.setState({
         visible : true,
-        
       });
   }
 
@@ -31,28 +41,48 @@ export default class ViewPlace extends Component {
       });
   }
  
+  queryPlaceDetails(placeId) {
+    const googleAPI = this.props.googleAPI;
+    let service;
+    let place;
+    var request = {
+          placeId: placeId
+        };
+    service = new googleAPI.places.PlacesService(document.createElement('div.placeDetailsAttrib'));
+    let callback = (place, status) => {
+      if (status == googleAPI.places.PlacesServiceStatus.OK) {
+        console.log(place);
+        this.setState({
+          detailedPlace: place
+        })
+      }
+    }
+    service.getDetails(request, callback);
+  }
 
   render() {
+    const place = this.state.detailedPlace;
     return (
       <section align="left">
         <input 
-        	type="button" 
-        	value="More Details"
+          type="button" 
+          value="More Details"
           id={this.props.placeId} 
-        	onClick={() => this.openModal()} 
+          onClick={() => this.openModal()} 
         />
         <Modal 
-        	visible={this.state.visible} 
-        	width="60%" 
-        	height="80%" 
-        	effect="fadeInUp" 
-        	onClickAway={() => this.closeModal()}>
+          visible={this.state.visible} 
+          width="80%" 
+          height="90%" 
+          effect="fadeInUp" 
+          onClickAway={() => this.closeModal()}>
           <div>
-            <h1>Place Name</h1>
-
+            <h1>{place.name}</h1>
+            <h4>{place.formatted_address}</h4>
+            
           </div>
         </Modal>
       </section>
-  	);
+    );
   }
 }
