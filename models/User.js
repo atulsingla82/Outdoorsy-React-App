7fb60f11088a1a948605bfc70ddf410b8b0ea450
C. Reid <ctlnrd@gmail.com>
2017-07-25 00:37:45 -0400

    models update

--- models/User.js
+++ models/User.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+const bcrypt = require('bcrypt');
 
-const UserSchema = new Schema({
+const UserSchema = new mongoose.Schema({
 	firstname: {
 		type: String, 
 		select: false,
@@ -13,7 +12,7 @@ const UserSchema = new Schema({
 		type: String, 
 		required: false
 	},
-	emailaddress: {
+	email: { //changed from 'emailaddress'
 		type: String, 
 		unique: true,
 		match: [/.+\@.+\..+/, "Please enter a valid e-mail address"],
@@ -32,9 +31,39 @@ const UserSchema = new Schema({
 	}
 });
 
-UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("User", UserSchema);
+/**
+ * Compare the passed password with the value in the database. A model method.
+ *
+ * @param {string} password
+ * @returns {object} callback
+ */
+UserSchema.methods.comparePassword = function comparePassword(password, callback) {
+  bcrypt.compare(password, this.password, callback);
+};
+
+/**
+ * The pre-save hook method.
+ */
+UserSchema.pre('save', function saveHook(next) {
+  const user = this;
+
+  // proceed further only if the password is modified or the user is new
+  if (!user.isModified('password')) return next();
+
+  return bcrypt.genSalt((saltError, salt) => {
+    if (saltError) { return next(saltError); }
+
+    return bcrypt.hash(user.password, salt, (hashError, hash) => {
+      if (hashError) { return next(hashError); }
+
+      // replace a password string with hash value
+      user.password = hash;
+
+      return next();
+    });
+  });
+});
 
 
-
+module.exports = mongoose.model('User', UserSchema);
