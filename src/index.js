2e4ec304a73d75fde9140b10672ae6402fdb4c76
C. Reid <ctlnrd@gmail.com>
2017-07-25 12:59:28 -0400

    clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Router, Route, hashHistory } from 'react-router';
-// import './styles/index.css';
 import App from './App';
 
 
@@ -11,12 +9,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
 
-
-// ReactDOM.render(
-//   <Router history={hashHistory}>
-//     <Route path="/test" component={App}/>
-//     <Route path="*" component={Whoops404}/>
-//   </Router>, 
-// document.getElementById('root'));
-// registerServiceWorker();
 
